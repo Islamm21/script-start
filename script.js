932b84714ecaca6cd8e0667ec88c039275c839b8
Islom <islom2207.icloud.com>
2022-11-17 21:09:42 +0300

    header

---
+++ script.js
@@ -0,0 +1,36 @@
+// var age= 25
+// console.log(age);
+
+// var a=2
+// var b=10 
+// var c=a+b
+// console.log(C);
+
+//  console.log(typeof 22); //son
+//  console.log(typeof "22"); //matn
+//  console.log(typeof true); //boolean
+
+//  var ism = "Nodir"
+//  var age = 17
+//  console.log("sening isming " + ism + " sening yoshing: " + age);
+
+
+
+
+
+
+
+var ism = prompt ("Ismingiz nima ?")
+console.log ("Mening ismim: " + ism);
+var age = +prompt("Yoshingiz nechida ?")
+console.log ("Mening yoshim: " + age)
+var misol1 = +prompt ("6+4=?")
+console.log("To'g'ri javob: "+ misol1)
+var misol2 = +prompt ("21/3=?")
+console.log("To'g'ri javob: "+ misol2);
+var misol3 = +prompt ("14-2=?")
+console.log("To'g'ri javob: " + misol3);
+var misol4 = +prompt ("5*5=?")
+console.log("To'g'ri javob: "+ misol4)
+var misol5 = +prompt ("15%2=?")
+console.log("To'g'ri javob: "+ misol5)
